1c4a9da53c62637fbbd2f376ddafaf29a561bb9a
Bruno Batista Soares <brunnobsoaares@gmail.com>
2025-06-30 15:14:50 -0300

    feat: Move celery.py to project root and update paths

--- pmo_api/__init__.py
+++ pmo_api/__init__.py
@@ -1,7 +1,9 @@
-# pmo_api/pmo_api/__init__.py
+# pmo_api/__init__.py (dentro da pasta pmo_api principal)
 
 # Isto irá garantir que a app do Celery seja sempre importada quando
 # o Django iniciar para que as tarefas partilhadas (@shared_task) usem esta app.
-from .celery import app as celery_app
+# Note que aqui estamos importando 'celery_app' do módulo 'celery' que está na raiz
+from . import celery as celery_module # Importa o modulo celery da raiz
+celery_app = celery_module.app
 
 __all__ = ('celery_app',)
